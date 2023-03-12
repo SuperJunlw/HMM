99017f9a8d96719f9da15380995ffc25dd013a45
Jun Wu <60298jw@gmail.com>
2023-03-11 23:34:36 -0800

    Update FileIO.java to preprocess training set and test set

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,23 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Driver {
 
     public static void main(String[] args) throws IOException {
+        int trainAmount = 1100;
+        int testAmount = 200;
+        List<Integer> testList = new ArrayList<>();
+        int[] obs = FileIO.readFolder(trainAmount, testAmount, testList);
+
+        int[] zaTestSet = new int[testList.size()];
+        for(int i = 0; i < zaTestSet.length; i++) {
+            zaTestSet[i] = testList.get(i);
+        }
+
+        System.out.print(Arrays.toString(zaTestSet));
+        System.out.print(zaTestSet.length);
         /*
         int N = 0;
         int M = 0;
@@ -21,6 +36,6 @@ public class Driver {
         hmmTest.forwardAlogrthm();
         hmmTest.calculateScore();
          */
-        System.out.print(FileIO.readZbot());
+
     }
 }
--- src/FileIO.java
+++ src/FileIO.java
@@ -4,18 +4,55 @@ import java.util.*;
 
 public class FileIO {
 
-    public static int readZbot() throws IOException {
-        List<String> list = new ArrayList<>();
-        int count = 0;
-        File folder = new File("zbot");
+    //zbot folder has 2136 files
+    //winwebsec folder has 4360 files
+    //zeroaccess folder has 1305 files
+    public static int[] readFolder(int trainAmount, int testAmount, List<Integer> testList) throws IOException {
+        Hashtable<String, Integer> hb = new Hashtable<>();
+        List<Integer> trainList = new ArrayList<>();
 
+        int index = 0, count = 0, num = 0;
+
+        File folder = new File("zeroaccess");
         File fileList[] = folder.listFiles();
         Scanner sc = null;
         for(File file : fileList) {
-
-            //Instantiating the Scanner class
+            sc= new Scanner(file);
+            String opCode;
+            if(count < trainAmount) {
+                while (sc.hasNextLine()) {
+                    opCode = sc.nextLine();
+                    num++;
+                    if (hb.containsKey(opCode)) {
+                        trainList.add(hb.get(opCode));
+                    } else {
+                        hb.put(opCode, index);
+                        trainList.add(index);
+                        index++;
+                    }
+                }
+            }
+            else if(count >= trainAmount && count < trainAmount + testAmount){
+                while (sc.hasNextLine()) {
+                    opCode = sc.nextLine();
+                    if (hb.containsKey(opCode)) {
+                        testList.add(hb.get(opCode));
+                    } else {
+                        hb.put(opCode, index);
+                        testList.add(index);
+                        index++;
+                    }
+                }
+            }
             count++;
         }
-        return count;
+        System.out.println(count);
+        System.out.println(num);
+
+        int[] obsTrain = new int[trainList.size()];
+        for(int i = 0; i < obsTrain.length; i++) {
+            obsTrain[i] = trainList.get(i);
+        }
+        return obsTrain;
     }
 }
